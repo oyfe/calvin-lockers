12e9f536c0ae5d3c618d4ee292e835fc141303ce
oyfe <131604299+oyfe@users.noreply.github.com>
2024-12-23 23:28:37 +0700

    Delete backend directory

--- backend/routes/lockerRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import { registerLocker, getAvailableLockers } from "../controllers/lockerController.js"
-const router = express.Router();
-
-router.post('/',registerLocker);
-router.get('/', getAvailableLockers);
-
-export default router;
--- backend/routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { registerUser } from '../controllers/userController.js';
-const router = express.Router();
-
-// Map POST /api/register to the registerUser function in userController
-router.post('/', registerUser);
-
-export default router 
